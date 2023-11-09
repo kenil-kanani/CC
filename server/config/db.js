c7bd671de76c7b5c8fc950457ceed6a9a7e8a786
Monil Prajapati <monillprajapatii@gmail.com>
2023-11-09 21:19:28 +0530

    mongo error solved

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const USERNAME = "process.env.USERNAME";
-  const PASSWORD = "process.env.PASSWORD";
+  const USERNAME = process.env.MONGO_DB_USERNAME;
+  const PASSWORD = process.env.MONGO_DB_PASSWORD;
   try {
     const conn = await mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.hgdaqk5.mongodb.net/`);
     console.log(`Connected To Mongodb Database ${mongoose.connection.host}`);
